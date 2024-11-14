50a6f72210d407a4ecac104a1de33ecb92e360ea
guilherme <victorguilherme015@gmail.com>
2024-11-14 09:04:12 -0300

    feat: dashboard service

--- src/main/java/com/cesarschool/coffeeshop/controller/DashboardController.java
+++ src/main/java/com/cesarschool/coffeeshop/controller/DashboardController.java
@@ -2,6 +2,7 @@ package com.cesarschool.coffeeshop.controller;
 
 import com.cesarschool.coffeeshop.domain.DashboardSummary;
 import com.cesarschool.coffeeshop.repository.DashboardRepository;
+import com.cesarschool.coffeeshop.service.DashboardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class DashboardController {
 
     @Autowired
-    private DashboardRepository dashboardRepository;
+    private DashboardService dashboardService;
 
     @GetMapping("/dashboard")
     public DashboardSummary getDashboardSummary() {
-        return dashboardRepository.getDashboardSummary();
+        return dashboardService.getDashboardSummary();
     }
 }
